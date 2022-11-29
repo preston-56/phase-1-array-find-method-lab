48ce75bd8659cac3667518ba323a6dcfbfefc80e
Preston <prestonosoro56@gmail.com>
2022-11-29 21:40:26 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+//   Array.prototype.find()
+function superbowlWin (record){
+    const result = record.find(element => element.result==='W');
+    return result !== undefined ? result.year : result;
+};
+
+superbowlWin();
